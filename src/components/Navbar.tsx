43c72ef78ac958b9eeb5fd404fad0319380e709e
Jeff Chisambi <chisambijeffrey3@gmail.com>
2025-06-16 12:00:07 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar: React.FC = () => {
     { icon: Github, href: "https://github.com/jeffchisambi", label: "GitHub" },
     {
       icon: Linkedin,
-      href: "https://linkedin.com/in/jeffreychisambi",
+      href: "https://linkedin.com/in/Jeffrey Chisambi",
       label: "LinkedIn",
     },
     {
